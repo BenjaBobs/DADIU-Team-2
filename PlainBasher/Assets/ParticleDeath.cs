c71ca9cfab5e8c83706332d77857ce0c52f94582
bhuberDADIU <bhuber11@student.aau.dk>
2014-09-15 12:22:19 +0200

    Fixed particle memory leak

---
+++ PlainBasher/Assets/ParticleDeath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDeath : MonoBehaviour {
+
+    float deathTime = 2;
+	
+	// Update is called once per frame
+	void Update () {
+        deathTime -= Time.deltaTime;
+        if (deathTime <= 0)
+            Destroy(gameObject);
+	}
+}
