43dd8e51bd9fc04a8ea857e6bb3ed3f1ab1b9c67
Jonas K <jonaskaerlev@gmail.com>
2014-09-16 08:32:33 +0200

    Split language selection into two boxes

--- PlainBasher/Assets/Scripts/guiOptions.cs
+++ PlainBasher/Assets/Scripts/guiOptions.cs
@@ -35,30 +35,26 @@ public class guiOptions : MonoBehaviour {
 
     void OnGUI()
     {
+		float posx;
+		posx = Screen.width / 2;
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background, ScaleMode.ScaleToFit);
 
-        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 4 -100, 150, 30), "Toggle "+ Localization.instance.GetString(Localization.LocKey.Music));
-        music = GUI.Toggle(new Rect(Screen.width / 2 + 50 , Screen.height / 4 -100, 40, 40), music, "");
+		GUI.Label(new Rect(posx - 75, Screen.height / 4 -100, 150, 30), "Toggle "+ Localization.instance.GetString(Localization.LocKey.Music));
+        music = GUI.Toggle(new Rect(posx + 50 , Screen.height / 4 -100, 40, 40), music, "");
 
-        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 4 - 25, 150, 30), "Toggle " + Localization.instance.GetString(Localization.LocKey.Soundfx));
-        sound = GUI.Toggle(new Rect(Screen.width / 2 + 50, Screen.height / 4 -25  , 40, 40), sound, "");
+		GUI.Label(new Rect(posx - 75, Screen.height / 4 - 25, 150, 30), "Toggle " + Localization.instance.GetString(Localization.LocKey.Soundfx));
+        sound = GUI.Toggle(new Rect(posx + 50, Screen.height / 4 -25  , 40, 40), sound, "");
 
-        if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/4 + 50,200,200),flag))
+		if (GUI.Button(new Rect(posx - 100 - 150, Screen.height/4 + 50,200,200),flagUK))
         {
-            bflag = !bflag;
-            flag = bflag ? flagUK : flagDK;
-            if (bflag)
-            {
-                Localization.instance.SetLanguage(Localization.LocLanguage.English);
-            }
-            else
-            {
-                Localization.instance.SetLanguage(Localization.LocLanguage.Danish);
-            }
-            
+			Localization.instance.SetLanguage(Localization.LocLanguage.English);
         }
+		if (GUI.Button(new Rect(posx - 100 + 150, Screen.height/4 + 50,200,200),flagDK))
+		{
+			Localization.instance.SetLanguage(Localization.LocLanguage.Danish);
+		}
 
-        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 4 + 250, 100, 100), Localization.instance.GetString(Localization.LocKey.ReturnToMenu)))
+		if (GUI.Button(new Rect(posx- 75, Screen.height *0.8f, 150, 75), Localization.instance.GetString(Localization.LocKey.ReturnToMenu)))
         {
             //gameObject.AddComponent("guiStart");
             //Destroy(this);
