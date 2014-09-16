495a9a05aae0aee76cde9a022189b131cb7c06a4
Elias <eliasringhauge@hotmail.com>
2014-09-16 09:17:03 +0200

    GUI FFS

--- PlainBasher/Assets/Scripts/guiStart.cs
+++ PlainBasher/Assets/Scripts/guiStart.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class guiStart : MonoBehaviour {
     public static guiStart staticref;
     private Texture background;
-
+    private GUISkin skinMenu;
+    private int butonWidth = 200;
     void Awake()
     {
         staticref = this;
@@ -15,6 +16,7 @@ public class guiStart : MonoBehaviour {
 
         Settings.instance.SetPause(true);
         background = Resources.Load("GUI/loadingScreen") as Texture;
+        skinMenu = Resources.Load("GUI/GUIMenu") as GUISkin;
 	}
 	
 	// Update is called once per frame
@@ -24,39 +26,34 @@ public class guiStart : MonoBehaviour {
 
     void OnGUI()
     {
+        GUI.skin = skinMenu;
         //Le´background, cause it is awsome
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background, ScaleMode.ScaleToFit);
 
         //GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height/4, 100, 30), Localization.instance.GetString(Localization.LocKey.PlayerName));
         //playerName = GUI.TextField(new Rect(Screen.width / 2, Screen.height / 4 , 200, 30), playerName, 25);
-        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 4 +50, 100, 50), "START")) //localization ?
+        if (GUI.Button(new Rect(Screen.width / 2 - (butonWidth / 2), Screen.height / 4 + 50, butonWidth, 50), "START")) //no need for localization
         {
-			AudioManager.StopSplashMusic(true);
-			AudioManager.PlayButton();
             Settings.instance.SetPause(false);
-			AudioManager.PlayMusic();
-			this.enabled = false;
+            this.enabled = false;
         }
-        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 4 + 100, 100, 50), Localization.instance.GetString(Localization.LocKey.Scoreboard)))
+        if (GUI.Button(new Rect(Screen.width / 2 - (butonWidth / 2), Screen.height / 4 + 130, butonWidth, 50), Localization.instance.GetString(Localization.LocKey.Scoreboard).ToUpper()))
         {
-			AudioManager.PlayButton();
             //gameObject.AddComponent("guiScore");
             //Destroy(this);
             guiScore.staticRef.enabled = true;
             this.enabled = false;
         }
-        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 4 + 150, 100, 50), Localization.instance.GetString(Localization.LocKey.Options)))
+        if (GUI.Button(new Rect(Screen.width / 2 - (butonWidth / 2), Screen.height / 4 + 210, butonWidth, 50), Localization.instance.GetString(Localization.LocKey.Options).ToUpper()))
         {
-			AudioManager.PlayButton();
             //gameObject.AddComponent("guiOptions");
             //Destroy(this);
             guiOptions.staticref.enabled = true;
             this.enabled = false;
         }
 
-        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 4 + 200, 100, 50), Localization.instance.GetString(Localization.LocKey.Quit)))
+        if (GUI.Button(new Rect(Screen.width / 2 - (butonWidth / 2), Screen.height / 4 + 290, butonWidth, 50), Localization.instance.GetString(Localization.LocKey.Quit).ToUpper()))
         {
-			AudioManager.PlayButton();
             Application.Quit();
         }
     }
