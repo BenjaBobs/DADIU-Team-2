9ad8f9707fb518ee90d8343aa292307a4d720891
bhuberDADIU <bhuber11@student.aau.dk>
2014-09-17 09:12:14 +0200

    Cows

--- PlainBasher/Assets/CowHerd.cs
+++ PlainBasher/Assets/CowHerd.cs
@@ -6,6 +6,7 @@ public class CowHerd : MonoBehaviour {
 	public GameObject cow1;
 	public GameObject cow2;
 	public GameObject cow3;
+    public GameObject StealingJelly;
 	private int cow;
 	public static CowHerd instance;
 	private
@@ -45,16 +46,34 @@ public class CowHerd : MonoBehaviour {
 
 		if (cow == 1)
 		{
+            Vector3 pos = cow1.transform.position;
+            pos.y -= 2;
+            pos.x += 1;
+            pos.z += 1;
+            GameObject JLY = Instantiate(StealingJelly, pos, Quaternion.Euler(0,180,0)) as GameObject;
+            JLY.GetComponentInChildren<Animator>().SetTrigger("MunchMunch");
             cow1.GetComponentInChildren<Animator>().SetTrigger("Taken");
 			//cow1.SetActive(false);
 		}
 		if (cow == 2)
 		{
+            Vector3 pos = cow2.transform.position;
+            pos.y -= 2;
+            pos.x += 1;
+            pos.z += 1;
+            GameObject JLY = Instantiate(StealingJelly, pos, Quaternion.Euler(0, 180, 0)) as GameObject;
+            JLY.GetComponentInChildren<Animator>().SetTrigger("MunchMunch");
             cow2.GetComponentInChildren<Animator>().SetTrigger("Taken");
 			//cow2.SetActive(false);
 		}
 		if (cow == 3)
 		{
+            Vector3 pos = cow3.transform.position;
+            pos.y -= 2;
+            pos.x += 1;
+            pos.z += 1;
+            GameObject JLY = Instantiate(StealingJelly, pos, Quaternion.Euler(0, 180, 0)) as GameObject;
+            JLY.GetComponentInChildren<Animator>().SetTrigger("MunchMunch");
             cow3.GetComponentInChildren<Animator>().SetTrigger("Taken");
 			//cow3.SetActive(false);
 		}
